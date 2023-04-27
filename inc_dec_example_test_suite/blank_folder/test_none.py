350c9d04b4bbbbd44c4661aa6490939a483d081f
eleanorjboyd <eleanorboyd@microsoft.com>
2023-04-27 10:54:58 -0700

    remove

--- inc_dec_example_test_suite/blank_folder/test_none.py
+++
@@ -1,6 +0,0 @@
-
-def not_to_be_tested():
-    print("this function is not to be tested")
-
-if __name__ == "__main__":
-    print("hello world")
--- inc_dec_example_test_suite/test_single_function.py
+++
@@ -1,4 +0,0 @@
-import inc_dec
-
-def test_single_1f():
-    assert inc_dec.increment(-1) == 2
